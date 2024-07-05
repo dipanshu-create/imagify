057a13b0ef5fabe4effe09cc393aca05ec41813f
da185214 <da185214@ncr.com>
2024-07-05 23:50:50 +0530

    adding clerk

---
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -0,0 +1,8 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+const DastboardPage = () => {
+  return <p>Dashboard page (Protected)</p>;
+};
+
+export default DastboardPage;
